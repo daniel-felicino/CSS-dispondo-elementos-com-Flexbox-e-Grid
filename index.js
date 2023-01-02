149d247577fc1063a80069a9e2950c1a489e8c16
https://github.com/daniel-felicino <daniel-feliciano@hotmail.com>
2023-01-02 17:33:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// const botaoMenu = document.getElementById('botaoMenu')
+function botaoMenu() {
+
+    const menuLink = document.getElementById('menuLink')
+
+    menuLink.classList.toggle('menuLinkEscondido')
+//toggle Alterne entre adicionar e remover um nome de classe de um elemento com JavaScript.
+
+
+}
